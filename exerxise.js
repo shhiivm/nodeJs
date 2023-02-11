34610cc5e622cab5e53a75e2a5a3be0fe098f484
Shivam Singh <shhiivm@outlook.com>
2023-02-11 09:01:05 +0530

    basic changes

---
+++ exerxise.js
@@ -0,0 +1,12 @@
+// Exercise 1
+// 1) Create a folder name it Ex1
+// 2) Create a file into it bio.txt with some data.
+// 3) add more data into it, at the end of existing data.
+// 4) Read the data without getting buffer data at first.
+//             //Use File encoding
+// 5) Rename the file to mybio.txt
+// 6) now delete both the file and folder.
+const fs = require("fs");
+// fs.mkdirSync('ex1');
+// fs.writeFileSync('ex1/bio.txt',"Hi! my name is Shivanand Singh")
+fs.appendFileSync('ex1/bio.txt',", I am 24 and learning node js");
