eca7e506fd3f23407e1a853ca7962b76db3380c3
Shivam Singh <shhiivm@outlook.com>
2023-02-11 18:55:09 +0530

    Performed basic operations

--- exerxise.js
+++ exerxise.js
@@ -6,7 +6,23 @@
 //             //Use File encoding
 // 5) Rename the file to mybio.txt
 // 6) now delete both the file and folder.
+
+
 const fs = require("fs");
+
+
 // fs.mkdirSync('ex1');
+
 // fs.writeFileSync('ex1/bio.txt',"Hi! my name is Shivanand Singh")
-fs.appendFileSync('ex1/bio.txt',", I am 24 and learning node js");
+
+// fs.appendFileSync('ex1/bio.txt',", I am 24 and learning node js");
+
+// const data = fs.readFileSync("ex1/bio.txt","utf-8");
+
+// console.log(data);
+
+// fs.renameSync('ex1/bio.txt','ex1/mybio.txt');
+
+// fs.unlinkSync('ex1/mybio.txt');
+
+fs.rmdirSync('ex1');
