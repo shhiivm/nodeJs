26d678bd7e55be2d4b07d4c22a6ac68a8c6ef202
Shivam Singh <shhiivm@outlook.com>
2023-02-12 07:33:13 +0530

    Created user defined modules

---
+++ Create and Export Module/app.js
@@ -0,0 +1,14 @@
+const add = (a,b) =>{
+    return a+b;
+    }
+    const sub = (a,b)=>{
+        return a-b;
+    }
+    const mul = (a,b)=>{
+        return a*b;
+    }
+    
+    module.exports.add=add;
+    module.exports.sub=sub;
+    module.exports.mul=mul;
+    
