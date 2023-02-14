d935a45cd92d0a2742b27eb9a5bfcb7ba39d2e04
Shivam Singh <shhiivm@outlook.com>
2023-02-14 09:15:26 +0530

    Request pages added

--- httpServer/index.js
+++ httpServer/index.js
@@ -2,6 +2,17 @@
 // The request 
 const http = require("http");
 const server = http.createServer((req,res)=>{
+    // console.log(req.url);
+    if(req.url=='/'){
+        console.log("Hello This is Home page");
+    }
+    else if(req.url=='/About'){
+        console.log("Hello This is About page");
+    }
+    else if(req.url=='/contact'){
+        console.log("Hello This is contact page");
+    }
+
     res.end('Hello! from server Shivam');
 });
 server.listen(8000,"127.0.0.1",()=>{
