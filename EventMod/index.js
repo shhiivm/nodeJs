c7c392ffd0dc310597b48df7c4df5f3a3f4f016b
shhiivm <shhiivm@pm.me>
2023-02-18 01:12:18 -0800

    calling mmultiple functions by 1 event

--- EventMod/index.js
+++ EventMod/index.js
@@ -1,13 +1,23 @@
 //  Events Module
 // NodeJs has a built-in module, called 'Events', where you can create-, fire-, and listen for events.
 
+//eg-1 Registering for the event to be fired only one time
+//eg-2 Create an event emitter instance and register a couple of callbacks.
+//eg-3 Registering for the event with callbacks parameters.
+
 
 const EventsEmitter = require('events'); //EventsEmitter is an object
 
 const event = new EventsEmitter(); // events is the object of class EventsEmitter
 
 event.on('myName',()=>{
-    console.log("Shivam");
+    console.log("Shiv");
+});
+event.on('myName',()=>{
+    console.log("Anand");
+});
+event.on('myName',()=>{
+    console.log("Singh");
 });
 
 event.emit('myName');
